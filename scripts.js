76dcea9e92234232552dd038ea354941abc9a44d
Ilya Yakubovich <ilya@Ilya-Yakubovichs-MacBook-Pro.local>
2011-09-07 19:36:32 -0400

    Hover effects!

--- scripts.js
+++ scripts.js
@@ -1,7 +1,13 @@
 $(document).ready(function(){
-  $("a").click(function(event){
 
+  $("li").fadeTo("fast", 0.6);
 
-
-  });
+  $("li").hover(
+    function(event){
+      $(this).fadeTo("fast", 1);
+    },
+    function(event){
+      $(this).fadeTo("fast", 0.6);
+    }
+  );
 });
