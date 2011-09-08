584a11c97163936b1ac32e7b36332d140458a9d6
Ilya Yakubovich <ilya.yakubovich@gmail.com>
2011-09-07 21:44:31 -0400

    Added resume dir, and increased default icon brightness

--- scripts.js
+++ scripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
-  $("li").fadeTo("fast", 0.6);
+  $("li").fadeTo("fast", 0.8);
 
   $("li").hover(
     function(event){
       $(this).fadeTo("fast", 1);
     },
     function(event){
-      $(this).fadeTo("fast", 0.6);
+      $(this).fadeTo("fast", 0.8);
     }
   );
 });
